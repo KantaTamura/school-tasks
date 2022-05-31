fb701780ce648d5c45726de2368765a054eee824
KantaTamura <tkanta496@gmail.com>
2022-05-31 13:52:38 +0900

    wip

---
+++ exer-c/task2/simple2-talk-client.c
@@ -0,0 +1,88 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PORT 10130
+
+int main(int argc, char** argv) {
+    /* 入力エラー */
+    if (argc != 3) {
+        fprintf(stderr, "no command line args\n");
+        exit(-1);
+    }
+    /* ソケットの生成 */
+    int server_socket;
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+    /* serverの情報設定 */
+    struct sockaddr_in server;
+    bzero(&server, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    struct hostent* hp;
+    if ((hp = gethostbyname(argv[1])) == NULL) {
+        fprintf(stderr, "unknown server %s\n", argv[1]);
+        exit(1);
+    }
+    bcopy(hp->h_addr_list[0], &server.sin_addr, hp->h_length);
+    /* serverとの接続 */
+    if (connect(server_socket, (struct sockaddr*)&server, sizeof(struct sockaddr_in)) == -1) {
+        fprintf(stderr, "can't connect %s\n", argv[1]);
+        exit(-1);
+    }
+    /* 名前を交換 */
+    char server_name[128];
+    write(server_socket, argv[2], sizeof(argv[2]));
+    if (read(server_socket, server_name, sizeof(server_name)) < 0) {
+        perror("read");
+        fprintf(stderr, "can't read server name\n");
+        exit(-1);
+    }
+    printf("%s connected\n", server_name);
+    /* select()の準備 */
+    fd_set rfds;
+    struct timeval tv;
+    /* select()が監視する時間設定 */
+    tv.tv_sec = 1;
+    tv.tv_usec = 0;
+    /* 処理 */
+    for (;;) {
+        /* select()が監視する対象 */
+        FD_ZERO(&rfds);
+        FD_SET(0, &rfds);
+        FD_SET(server_socket, &rfds);
+        /* stdio client_socketが受付可能か判断 */
+        if (select(server_socket + 1, &rfds, NULL, NULL, &tv) > 0) {
+            /* stdin */
+            if (FD_ISSET(0, &rfds)) {
+                /* 送信文字列受け取り */
+                char str[256] = "";
+                if (scanf("%255[^\n]%*[^\n]", str) == EOF) break;
+                scanf("%*c");
+                /* serverに送信 */
+                write(server_socket, str, sizeof(str));
+            }
+            /* client_socket */
+            if (FD_ISSET(server_socket, &rfds)) {
+                char str[256] = "";
+                int nbytes;
+                if ((nbytes = read(server_socket, str, sizeof(str))) < 0) {
+                    perror("read");
+                    break;
+                }
+                else if (nbytes == 0) break;
+                printf("%s\n", str);
+            }
+        }
+    }
+    close(server_socket);
+    return 0;
+}
---
+++ exer-c/task2/simple2-talk-server.c
@@ -0,0 +1,101 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PORT 10130
+
+int main(int argc, char** argv) {
+    /* ソケットの生成 */
+    int server_socket;
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+    /* ソケットアドレス再利用の設定 */
+    int reuse = 1;
+    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+        perror("setsockopt");
+        exit(1);
+    }
+    /* client 受付用ソケットの情報設定 */
+    struct sockaddr_in server;
+    bzero(&server, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(PORT);
+    /* ソケットにソケットアドレスを割り当てる */
+    if (bind(server_socket, (struct sockaddr*)&server, sizeof(server)) < 0) {
+        perror("bind");
+        exit(1);
+    }
+    /* 待ち受けクライアント数の設定 */
+    if (listen(server_socket, 5) < 0) {
+        perror("listen");
+        exit(1);
+    }
+    do {
+        int nbytes = 256;
+        /* クライアントの受付 */
+        int client_socket;
+        struct sockaddr_in client;
+        int clen = sizeof(client);
+        if ((client_socket = accept(server_socket, (struct sockaddr*)&client, &clen)) < 0) {
+            perror("accept");
+            exit(2);
+        }
+        /* クライアントのホスト情報の取得 */
+        struct hostent* cp;
+        cp = gethostbyaddr((char*)&client.sin_addr, sizeof(struct in_addr), AF_INET);
+        /* 名前を交換 */
+        char client_name[128];
+        if (read(client_socket, client_name, sizeof(client_name)) < 0) {
+            perror("read");
+            fprintf(stderr, "can't read sclient name\n");
+            exit(-1);
+        }
+        write(server_socket, argv[1], sizeof(argv[1]));
+        printf("%s connected\n", client_name);
+        /* select()の準備 */
+        fd_set rfds;
+        struct timeval tv;
+        /* select()が監視する時間設定 */
+        tv.tv_sec = 1;
+        tv.tv_usec = 0;
+        do {
+            /* select()が監視する対象 */
+            FD_ZERO(&rfds);
+            FD_SET(0, &rfds);
+            FD_SET(client_socket, &rfds);
+            /* stdio client_socketが受付可能か判断 */
+            if (select(client_socket + 1, &rfds, NULL, NULL, &tv) > 0) {
+                /* stdin */
+                if (FD_ISSET(0, &rfds)) {
+                    /* 送信文字列受け取り */
+                    char str[256] = "";
+                    if (scanf("%255[^\n]%*[^\n]", str) == EOF) break;
+                    scanf("%*c");
+                    /* serverに送信 */
+                    nbytes = write(client_socket, str, sizeof(str));
+                }
+                /* client_socket */
+                if (FD_ISSET(client_socket, &rfds)) {
+                    char str[256] = "";
+                    if ((nbytes = read(client_socket, str, sizeof(str))) < 0) {
+                        perror("read");
+                        break;
+                    }
+                    else if (nbytes == 0) break;
+                    printf("%s\n", str);
+                }
+            }
+        } while (nbytes != 0);
+        close(client_socket);
+        printf("closed\n");
+    } while (1);
+}
