4177e732cd25c7556d1975f0f52257229fe10300
KantaTamura <tkanta496@gmail.com>
2022-06-20 14:54:40 +0900

    wip

--- exer-c/task4/chatclient.c
+++ exer-c/task4/chatclient.c
@@ -1,10 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+
+const int port = 10140;
 
 int main(int argc, char** argv) {
     if (argc != 3) {
         fprintf(stderr, "Usage: %s hostname username\n", argv[0]);
         exit(-1);
     }
+    // TODO: delete
     printf("hostname : %s, username : %s\n", argv[1], argv[2]);
+    
+    int server_socket;
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    struct sockaddr_in server;
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+
+    struct hostent* hp;
+    if ((hp = gethostbyname(argv[1])) == NULL) {
+        fprintf(stderr, "unknown server %s\n", argv[1]);
+        exit(1);
+    }
+    memcpy(&server.sin_addr, hp->h_addr_list[0], hp->h_length);
+
+    if (connect(server_socket, (struct sockaddr*)&server, sizeof(struct sockaddr_in)) == -1) {
+        fprintf(stderr, "can't connect %s\n", argv[1]);
+        exit(-1);
+    }
+    // TODO: delete
+    printf("connected to %s\n", argv[1]);
 }
